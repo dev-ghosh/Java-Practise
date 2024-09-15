333b1fff99a4712ada4c6913448c4a01818cd162
Your Name <dev.work616@gmail.com>
2024-09-15 11:12:53 +0530

    basics

---
+++ src/BasicsOfJava/basics.java
@@ -0,0 +1,11 @@
+package BasicsOfJava;
+
+public class basics {
+    public static void main(String[] args) {
+        System.out.println("hello dev"); //ln means enter to come in next line
+        System.out.println("mait ");
+        System.out.print("hello "); //one space will create a single space
+        System.out.print("ipu");  //no ln means will come in same line
+
+    }
+}
