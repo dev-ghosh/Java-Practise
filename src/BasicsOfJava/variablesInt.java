25bc6bc769ac0e60d1e8c0c8e6997a8b81c51ce9
Your Name <dev.work616@gmail.com>
2024-09-16 11:37:49 +0530

    basics

---
+++ src/BasicsOfJava/variablesInt.java
@@ -0,0 +1,11 @@
+package BasicsOfJava;
+
+public class variablesInt {
+    public static void main(String[] args) {
+        int x;
+        x=5;
+        System.out.println(x);
+        x=10; //updation
+        System.out.println(x);
+    }
+}
