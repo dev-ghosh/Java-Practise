af4d1de16a005c8acfd43c7054b57f5892fc19a8
Your Name <dev.work616@gmail.com>
2024-09-16 11:34:18 +0530

    basics

---
+++ src/BasicsOfJava/PrintingNo.java
@@ -0,0 +1,8 @@
+package BasicsOfJava;
+
+public class PrintingNo {
+    public static void main(String[] args) {
+        System.out.println("4+3"); //inverted commas so text
+        System.out.println(4+3); //no comma means value
+    }
+}
