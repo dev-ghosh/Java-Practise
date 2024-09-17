59832cad675468a37d64b575f7148c0123b4bc99
Your Name <dev.work616@gmail.com>
2024-09-17 19:31:09 +0530

    basics

---
+++ src/BasicsOfJava/TextAndVariable.java
@@ -0,0 +1,9 @@
+package BasicsOfJava;
+
+public class TextAndVariable {
+    public static void main(String[] args) {
+        int x=5,y=9,z=7;
+        System.out.println("the value of x is "+x);
+        System.out.println("value of y is "+y);
+    }
+}
