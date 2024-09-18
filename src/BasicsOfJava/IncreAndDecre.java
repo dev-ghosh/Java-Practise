e94ec18a2b382af1be06dc937eab7cd72fcfc338
Your Name <dev.work616@gmail.com>
2024-09-18 15:08:25 +0530

    basics

---
+++ src/BasicsOfJava/IncreAndDecre.java
@@ -0,0 +1,14 @@
+package BasicsOfJava;
+
+public class IncreAndDecre {
+    public static void main(String[] args) {
+        int x=5;
+        System.out.println(x);
+        x++; //x++ or x=x+1 same thing
+        System.out.println(x);
+        x=6;
+        System.out.println(x);
+        x--; //x-- or x=x-1 same thing
+        System.out.println(x);
+    }
+}
