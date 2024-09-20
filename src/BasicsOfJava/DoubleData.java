b64463cfb28c48105b997a09cee0e4c5b80a07af
Your Name <dev.work616@gmail.com>
2024-09-20 20:14:42 +0530

    basics

---
+++ src/BasicsOfJava/DoubleData.java
@@ -0,0 +1,10 @@
+package BasicsOfJava;
+
+public class DoubleData {
+    public static void main(String[] args) {
+        double x=5;
+        double y=2;
+        System.out.println(x/y);
+        System.out.println(5/2);
+    }
+}
