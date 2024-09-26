eed231e66c186de8388a2dec289463b71c0a2901
Your Name <dev.work616@gmail.com>
2024-09-27 00:15:07 +0530

    basics

---
+++ src/Loops/forloop.java
@@ -0,0 +1,12 @@
+package Loops;
+import java.util.Scanner;
+public class forloop {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a no : ");
+        int n=sc.nextInt();
+        for(int i=1;i<=6;i++){
+            System.out.println(i);
+        }
+    }
+}
