2aa6607237015e1e0ec85f867a66bc71993ce68d
Your Name <dev.work616@gmail.com>
2024-10-05 22:54:51 +0530

    basics

---
+++ src/JavaMethods/StdLibMethod.java
@@ -0,0 +1,9 @@
+package JavaMethods;
+
+public class StdLibMethod {
+    public static void main(String[] args) {
+        System.out.println(Math.sqrt(24));
+        System.out.println(Math.floor(5.3));
+        System.out.println(Math.ceil(5.3));
+    }
+}
