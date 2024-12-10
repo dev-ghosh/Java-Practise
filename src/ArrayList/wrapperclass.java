64b202e6d351b1cfa9054b13d86fb8995d1137fc
Your Name <dev.work616@gmail.com>
2024-12-11 00:41:59 +0530

    basics

---
+++ src/ArrayList/wrapperclass.java
@@ -0,0 +1,11 @@
+package ArrayList;
+
+public class wrapperclass {
+    // demonstration of wrapper class
+    public static void main(String[] args) {
+        Integer i=Integer.valueOf(4);
+        System.out.println(i);
+        Float f=Float.valueOf(4.567f); // focus on f
+        System.out.println(f);
+    }
+}
