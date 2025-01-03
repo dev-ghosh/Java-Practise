7a06724e2fc822c5ca22328dd094b34d5eab74cf
Your Name <dev.work616@gmail.com>
2025-01-04 03:05:08 +0530

    basics

---
+++ src/Recursion/OneToN.java
@@ -0,0 +1,20 @@
+package Recursion;
+import java.util.Scanner;
+public class OneToN {
+     static void printinc(int n){
+        if(n==1){                 // protects stack overflow // base case
+            System.out.println(1);
+            return ;
+        }
+        printinc(n-1); // 1,2,3....n-1,n //  recursive work/sub work
+         System.out.println(n); // self work
+    }
+
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter number n : ");
+    int n=sc.nextInt();
+    printinc(n);
+
+    }
+}
