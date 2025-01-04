eb2285e7d124bf7e31d38a9d6107ab6485b6379b
Your Name <dev.work616@gmail.com>
2025-01-05 05:01:17 +0530

    basics

---
+++ src/Recursion/FactorialN.java
@@ -0,0 +1,21 @@
+package Recursion;
+import java.util.Scanner;
+public class FactorialN {
+    static int factorial(int n){
+       if(n==0) return 1; // base case
+//        //recursive work/sub prob
+//        int smallans=factorial(n-1);
+//        //self work
+//        int ans=n*smallans;
+//        return ans;
+        // OR
+        return n*factorial(n-1); // n is self n-1 is sub work
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter n : ");
+        int n=sc.nextInt();
+        System.out.println(factorial(n));
+       // instead of import it can be sout(factorial(5));
+    }
+}
