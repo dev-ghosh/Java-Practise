7a9040be3638bee9b45d1f1072de8fa2c47e54c5
Your Name <dev.work616@gmail.com>
2025-01-06 03:50:46 +0530

    basics

---
+++ src/Recursion/FibonacciNo.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+public class FibonacciNo {
+    static int fib(int n){
+        if(n==0 || n==1) return n; // base case
+        //sub prob
+        int prev=fib(n-1);
+        int prevprev=fib(n-2);
+        //self work
+        return prev+prevprev;
+    }
+    public static void main(String[] args) {
+        System.out.println(fib(6));
+    }
+}
