fbc3e7da3ebd6a530b24315a1c94fa8e599d83e4
Dev Ghosh <dev.work616@gmail.com>
2025-01-15 20:05:51 +0530

    Update FibonacciNo.java

--- src/Recursion/FibonacciNo.java
+++ src/Recursion/FibonacciNo.java
@@ -3,7 +3,7 @@ package Recursion;
 public class FibonacciNo {
     static int fib(int n){
         if(n==0 || n==1) return n; // base case
-        //sub prob
+        //sub problem
         int prev=fib(n-1);
         int prevprev=fib(n-2);
         //self work
