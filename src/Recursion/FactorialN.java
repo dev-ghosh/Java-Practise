af264ee00a80f5d75fe31d24c7de808798936683
Dev Ghosh <dev.work616@gmail.com>
2025-02-07 23:21:10 +0530

    Update FactorialN.java

--- src/Recursion/FactorialN.java
+++ src/Recursion/FactorialN.java
@@ -5,7 +5,7 @@ public class FactorialN {
        if(n==0) return 1; // base case
 //        //recursive work/sub problem
 //        int smallans=factorial(n-1);
-//        //self workkkkkkk
+//        //self workkkkkkkk
 //        int ans=n*smallans;
 //        return ans;
         // OR
