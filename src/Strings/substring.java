08a2d4bf10318355b10372e37e3c0f91b8b27bf2
Your Name <dev.work616@gmail.com>
2025-04-20 14:40:56 +0530

    basics

---
+++ src/Strings/substring.java
@@ -0,0 +1,10 @@
+package Strings;
+import java.util.*;
+public class substring {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str="abcde";
+        System.out.println(str.substring(0,2));
+        System.out.println(str.substring(2));
+    }
+}
