858bd08091067208c9407d751f21e93106163b8c
Dev Ghosh <175207023+dev-ghosh@users.noreply.github.com>
2025-11-08 23:59:16 +0530

    Update FactorialN.java

--- src/Recursion/FactorialN.java
+++ src/Recursion/FactorialN.java
@@ -5,7 +5,7 @@ public class FactorialN {
        if(n==0) return 1; // base case
 //        //recursive work/sub problem
 //        int smallans=factorial(n-1);
-//        //self workkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+//        //self workkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
 //        int ans=n*smallans;
 //        return ans;
         // OR
